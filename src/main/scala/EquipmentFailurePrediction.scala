bfc2b9662ababcd81aed80b463e7ea552257abe0
neeha-ravi <nravi01@calpoly.edu>
2024-11-21 12:03:24 -0800

    verify setup

--- src/main/scala/EquipmentFailurePrediction.scala
+++ src/main/scala/EquipmentFailurePrediction.scala
@@ -1,5 +1,13 @@
-object EquipmentFailurePrediction {
+import org.apache.spark.sql.SparkSession
+
+object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, Scala Project!")
+    val spark = SparkSession.builder()
+      .appName("Equipment Failure Prediction")
+      .master("local[*]") // Use local mode with all cores
+      .getOrCreate()
+
+    println("Spark Session Initialized!")
+    spark.stop()
   }
 }

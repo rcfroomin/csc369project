f632cf28d42a957c4516b4a33d8ad4039720f3ac
Neeha Ravi <94156134+neeha-ravi@users.noreply.github.com>
2024-12-03 14:24:07 -0800

    Update Evaluator.scala

--- src/main/scala/Evaluator.scala
+++ src/main/scala/Evaluator.scala
@@ -1,17 +1,20 @@
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.sql.DataFrame
-
 object Evaluator {
-  def evaluate(predictions: DataFrame): Unit = {
-    val evaluator = new MulticlassClassificationEvaluator()
-      .setLabelCol("indexedLabel")
-      .setPredictionCol("prediction")
-
-    val accuracy = evaluator.setMetricName("accuracy").evaluate(predictions)
+  def evaluate(predictions: Array[(Int, Int)]): Unit = {
+    val accuracy = predictions.count { case (actual, predicted) => actual == predicted }.toDouble / predictions.length
     println(s"Accuracy: $accuracy")
 
-    val f1 = evaluator.setMetricName("f1").evaluate(predictions)
-    println(s"F1 Score: $f1")
+    val labelCounts = predictions.groupBy(_._1).mapValues(_.length)
+    val f1Scores = predictions.groupBy(_._1).map { case (label, instances) =>
+      val tp = instances.count { case (actual, predicted) => actual == predicted }
+      val fp = predictions.count { case (_, predicted) => predicted == label } - tp
+      val fn = labelCounts(label) - tp
+
+      val precision = if (tp + fp > 0) tp.toDouble / (tp + fp) else 0.0
+      val recall = if (tp + fn > 0) tp.toDouble / (tp + fn) else 0.0
+      val f1 = if (precision + recall > 0) 2 * (precision * recall) / (precision + recall) else 0.0
+      f1
+    }
+
+    println(s"F1 Scores: $f1Scores")
   }
 }
-

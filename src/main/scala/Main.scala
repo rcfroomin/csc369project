13dd4fef66c2bdcf51410d62f651da27ffc69f67
Neeha Ravi <94156134+neeha-ravi@users.noreply.github.com>
2024-12-03 14:24:18 -0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,21 @@
-import org.apache.spark.sql.SparkSession
-
 object Main {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
-      .appName("Predictive Maintenance")
-      .config("spark.driver.bindAddress", "127.0.0.1")
-      .getOrCreate()
-
-    val trainData = spark.read
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .csv("train_data.csv")
+    val trainDataPath = "train_data.csv"
+    val data = DataLoader.loadData(trainDataPath)
 
     val featureColumns = Array(
       "setting_1", "setting_2", "setting_3",
       "s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10",
       "s_11", "s_12", "s_13", "s_14", "s_15", "s_16", "s_17", "s_18", "s_19", "s_20", "s_21"
     )
+    val labelColumn = "RUL"
 
-    val trainDataProcessed = Preprocessor.transform(trainData, featureColumns)
+    val (features, labels) = Preprocessor.transform(data, featureColumns, labelColumn)
 
-    println("Processed Training Data:")
-    trainDataProcessed.show(5)
+    val trainingData = features.zip(labels)
+    val model = NaiveBayesTrainer.train(trainingData)
 
-    spark.stop()
+    val predictions = features.map(f => model.predict(f))
+    Evaluator.evaluate(labels.zip(predictions))
   }
 }

1fae303255249416cc6bea9c1a7e4b1a6cda8df9
rcfroomin <rcfroomin@gmail.com>
2024-12-02 18:58:19 -0800

    discretized the RUL into 3 classes

--- src/main/scala/EquipmentFailurePrediction.scala
+++ src/main/scala/EquipmentFailurePrediction.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -7,8 +8,16 @@ object Main {
       .master("local[*]") // Use local mode with all cores
       .getOrCreate()
 
-    println("Spark Session Initialized!")
+    val filePath = "train_data.csv"
+
+    val data = spark.read.option("header", "true").csv(filePath)
+
+    val discretizedData = data.withColumn("RUL_Class", when(col("RUL") <= 50, "Low")
+      .when(col("RUL") > 50 && col("RUL") <= 100, "Medium")
+      .otherwise("High"))
+
+    discretizedData.show()
+
     spark.stop()
   }
 }
-
